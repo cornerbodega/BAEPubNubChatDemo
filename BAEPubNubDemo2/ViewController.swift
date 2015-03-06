d5d2af0db616deda8f062e46a7582083d6bdde1b
Cornerbodega <merhone@gmail.com>
2015-03-05 23:16:05 -0800

    Initial Commit

---
+++ BAEPubNubDemo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BAEPubNubDemo2
+//
+//  Created by Dr. Marv on 3/5/15.
+//  Copyright (c) 2015 Dr. Marv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
